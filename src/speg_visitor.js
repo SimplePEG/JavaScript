da352c294c72435c88a0a92cb78a561584dea4f1
Oleksii Okhrymenko <ai_boy@live.ru>
2016-12-19 20:50:31 +0500

    removed unused pegs visitor

--- src/speg_visitor.js
+++ src/speg_visitor.js
@@ -1,41 +1,5 @@
 var rd = require('./rd_parser');
 
-function PEGJS_visitor(){}
-
-PEGJS_visitor.visit = function(node) {
-    return this[node.type](node);
-};
-PEGJS_visitor.prototype.string = function(node) {
-    return node.match;
-};
-PEGJS_visitor.prototype.regex_char = function(node) {
-    return node.match;
-};
-PEGJS_visitor.prototype.sequence = function(node) {
-    return node.children.map(function(child) { return this.visit(child); });
-};
-PEGJS_visitor.prototype.ordered_choice = function(node) {
-    return node.children.map(function(child) { return this.visit(child); });
-};
-PEGJS_visitor.prototype.zero_or_more = function(node) {
-    return node.children.map(function(child) { return this.visit(child); });
-};
-PEGJS_visitor.prototype.one_or_more = function(node) {
-    return node.children.map(function(child) { return this.visit(child); });
-};
-PEGJS_visitor.prototype.optional = function(node) {
-    return node.children.map(function(child) { return this.visit(child); });
-};
-PEGJS_visitor.prototype.and_predicate = function(node) {
-    return null;
-};
-PEGJS_visitor.prototype.not_predicate = function(node) {
-    return null;
-};
-PEGJS_visitor.prototype.end_of_file = function(node) {
-    return null;
-};
-
 function SPEG_actions_visitor() {
     this.actions = new SPEG_actions();
 }
@@ -145,6 +109,5 @@ SPEG_actions.prototype.parsing_end_of_file = function(node) {
 };
 
 module.exports = {
-    SPEG_actions_visitor: SPEG_actions_visitor,
-    PEGJS_visitor: PEGJS_visitor
+    SPEG_actions_visitor: SPEG_actions_visitor
 };
